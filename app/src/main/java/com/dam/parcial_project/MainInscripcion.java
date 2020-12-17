69a3299ed604ed757ed9acc6217cb8117f394a30
Jose Enrique <josehn.1466@gmail.com>
2020-12-17 16:45:19 -0500

    Estudiante 2 Huaycho

--- app/src/main/java/com/dam/parcial_project/MainInscripcion.java
+++ app/src/main/java/com/dam/parcial_project/MainInscripcion.java
@@ -2,13 +2,27 @@ package com.dam.parcial_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainInscripcion extends AppCompatActivity {
+
+    Button buttonRVolver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_inscripcion);
+
+        buttonRVolver = findViewById(R.id.buttonR);
+        buttonRVolver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainInscripcion.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
